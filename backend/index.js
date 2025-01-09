2d20206affcc173e99b6899cfd96b7a46565d133
Tushar <111505346+Tushar6396@users.noreply.github.com>
2025-01-09 22:01:51 +0530

    setup backend and database connection

---
+++ backend/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const connectDB = require('./db');
+
+dotenv.config();
+connectDB();
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
